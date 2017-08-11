59a39a3a049aeb4701a0efafd75c366ac267c88b
fuvce1 <catherine.fu@vce.com>
2017-08-11 13:30:53 -0500

    ESTS 130910 - vcenter-adapter: Exceptions are not being caught on GET /clusters. 20Gb log file   add error handing

--- dne-paqx/src/test/java/com/dell/cpsd/paqx/dne/service/task/handler/preprocess/FindVClusterTaskHandlerTest.java
+++ dne-paqx/src/test/java/com/dell/cpsd/paqx/dne/service/task/handler/preprocess/FindVClusterTaskHandlerTest.java
@@ -101,7 +101,7 @@ public class FindVClusterTaskHandlerTest
      * @since 1.0
      */
     @Test
-    public void testExecuteTask_successful_case() throws Exception {
+    public void testExecuteTask_successful_case() throws ServiceTimeoutException, ServiceExecutionException {
         ClusterInfo vCluster = new ClusterInfo("clusterTest1", 2);
         List<ClusterInfo> vClusters = new ArrayList<>();
         vClusters.add(vCluster);
@@ -130,7 +130,7 @@ public class FindVClusterTaskHandlerTest
      * @since 1.0
      */
     @Test
-    public void testExecuteTask_unsuccessful_case() throws Exception {
+    public void testExecuteTask_unsuccessful_case() throws ServiceTimeoutException, ServiceExecutionException {
         ClusterInfo vCluster = new ClusterInfo("clusterTest1", 2);
         List<ClusterInfo> vClusters = new ArrayList<>();
         vClusters.add(vCluster);
